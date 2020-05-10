004178569816ee248043fe8ab080f170d1a11014
christhetree <christhetree@gmail.com>
2020-05-10 20:44:10 +0900

    Patch loading with python27

--- python/decoder_speed.py
+++ python/decoder_speed.py
@@ -1,17 +1,17 @@
-from typing import Tuple, Any
-
 import cProfile
+
 import librenderman as rm
 import matplotlib.pyplot as plt
-
 
 SAMPLE_RATE = 44100
 BUFFER_SIZE = 512
 FFT_SIZE = 512
 SERUM_PATH = "/Library/Audio/Plug-Ins/VST/Serum.vst"
+# SERUM_PATH = "/Library/Audio/Plug-Ins/VST/SerumFX.vst"
+# SERUM_PATH = "/Users/christhetree/local_christhetree/research/titech/reverse_synthesis/python/angle_grinder.fxp"
 
 
-def setup_serum(path: str) -> Tuple[Any, Any]:
+def setup_serum(path: str) -> (rm.RenderEngine, rm.PatchGenerator):
     engine = rm.RenderEngine(SAMPLE_RATE, BUFFER_SIZE, FFT_SIZE)
     engine.load_plugin(path)
     generator = rm.PatchGenerator(engine)
@@ -37,12 +37,15 @@ def plot_audio(audio) -> None:
 
 
 def main() -> None:
-    num_trials = 10
+    num_trials = 0
     engine, generator = setup_serum(SERUM_PATH)
+
+    print(engine.get_plugin_parameters_description())
+    exit()
 
     for _ in range(num_trials):
         new_patch = get_random_patch(generator)
-        set_patch(engine, new_patch)
+        # set_patch(engine, new_patch)
 
         midi_note = 40
         midi_velocity = 127
@@ -56,4 +59,5 @@ def main() -> None:
 
 
 if __name__ == '__main__':
-    cProfile.run('main()')
+    main()
+    # cProfile.run('main()')
---
+++ python27/renderman_test.py
@@ -0,0 +1,27 @@
+import librenderman as rm
+
+SAMPLE_RATE = 44100
+BUFFER_SIZE = 512
+FFT_SIZE = 512
+SERUM_PATH = "/Library/Audio/Plug-Ins/VST/Serum.vst"
+
+
+def setup_serum(path):
+    engine = rm.RenderEngine(SAMPLE_RATE, BUFFER_SIZE, FFT_SIZE)
+    engine.load_plugin(path)
+    generator = rm.PatchGenerator(engine)
+    return engine, generator
+
+
+engine, generator = setup_serum(SERUM_PATH)
+print(engine.get_plugin_parameters_description())
+engine.load_preset('/Users/christhetree/local_christhetree/research/titech/audio/RenderMan/Builds/MacOSX/build/Debug/angle_grinder.fxp')
+
+midi_note = 40
+midi_velocity = 127
+note_length = 3.0
+render_length = 4.0
+
+
+engine.render_patch(midi_note, midi_velocity, note_length, render_length, False)
+audio = engine.get_audio_frames()
